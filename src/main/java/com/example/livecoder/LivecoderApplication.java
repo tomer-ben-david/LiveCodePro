583a9c5a1414a340aa37f4e9f3bd35c7979a3106
tomer-ben-david <tomerbd1@gmail.com>
2021-01-08 13:37:21 +0200

    initial commit

---
+++ src/main/java/com/example/livecoder/LivecoderApplication.java
@@ -0,0 +1,13 @@
+package com.example.livecoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivecoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivecoderApplication.class, args);
+	}
+
+}
